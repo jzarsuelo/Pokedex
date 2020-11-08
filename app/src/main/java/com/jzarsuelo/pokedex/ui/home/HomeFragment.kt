2f34e5b725a67b68662daeacd96e4b6f7bc5c3d2
Pao <jzarsuelo.dev@gmail.com>
2020-11-08 13:18:32 +1100

    Renamed PokemonCollectionFragment to HomeFragment

--- app/src/main/java/com/jzarsuelo/pokedex/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jzarsuelo/pokedex/ui/home/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.jzarsuelo.pokedex
+package com.jzarsuelo.pokedex.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jzarsuelo.pokedex.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PokemonCollectionFragment.newInstance] factory method to
+ * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PokemonCollectionFragment : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class PokemonCollectionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon_collection, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -49,7 +50,7 @@ class PokemonCollectionFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PokemonCollectionFragment().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
